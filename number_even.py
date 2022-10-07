eb66d2cd34efa8e1527e249e0666194ea44466ae
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-07 11:41:50 +0500

    number_even.py

--- number_even.py
+++ number_even.py
@@ -4,19 +4,19 @@
 
 #Print the number of even digits in the variable "var_int".
 var_int=1222
-sum=0
+
 n=var_int%10 #4
-sum+=(n+1)%2 #1
+s1=(n+1)%2 #1
 
 var_int=var_int//10 #123
 n=var_int%10 #3
-sum+=(n+1)%2 #0
+s2=(n+1)%2 #0
 
 var_int=var_int//10 #12
 n=var_int%10 #2
-sum+=(n+1)%2 #1
+s3=(n+1)%2 #1
 
 var_int=var_int//10 #1
 n = var_int
-sum+=(n+1)%2#0
-print(sum)
+s4=(n+1)%2#0
+print(s1+s2+s3+s4)
