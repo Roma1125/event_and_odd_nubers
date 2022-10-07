c55b362d8dd1244123c9b0afe982d488333a4f7c
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-07 13:15:40 +0500

    sum_even.py

--- sum_even.py
+++ sum_even.py
@@ -5,3 +5,20 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+var_int=1222
+sum=0
+n=var_int%10 #4
+sum+=(n+1)%2 *n #1
+
+var_int=var_int//10 #123
+n=var_int%10 #3
+sum+=(n+1)%2*n #0
+
+var_int=var_int//10 #12
+n=var_int%10 #2
+sum+=(n+1)%2*n #1
+
+var_int=var_int//10 #1
+n = var_int
+sum+=(n+1)%2*n#0
+print(sum)
