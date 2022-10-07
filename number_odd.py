b426a491960a21b787c6f93a45b3ca89e78ce992
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-07 11:48:04 +0500

    dsdjskdsdsk

--- number_odd.py
+++ number_odd.py
@@ -3,3 +3,20 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+var_int=1222
+sum=0
+n=var_int%10 #4
+sum+=(n)%2 #1
+
+var_int=var_int//10 #123
+n=var_int%10 #3
+sum+=(n)%2 #0
+
+var_int=var_int//10 #12
+n=var_int%10 #2
+sum+=(n)%2 #1
+
+var_int=var_int//10 #1
+n = var_int
+sum+=(n)%2#0
+print(sum)
